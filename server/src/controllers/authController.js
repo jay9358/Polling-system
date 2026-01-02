eee33220d05b587ac5c1d3fa217d5c40a5555ae2
jay9358 <jayaggarwal99288@gmail.com>
2026-01-03 02:03:03 +0530

    go

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -66,9 +66,9 @@ export const loginUser = async (req, res) => {
 
             res.cookie('jwt', token, {
                 httpOnly: true,
-                secure: process.env.NODE_ENV !== 'development',
-                sameSite: 'strict',
-                maxAge: 30 * 24 * 60 * 60 * 1000
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+                maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
             });
 
             res.json({
