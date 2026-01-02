61b7f39ec5b25bc99c137871a7c7e4d1208634a7
jay9358 <jayaggarwal99288@gmail.com>
2026-01-03 01:40:57 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -23,10 +23,7 @@ const io = new Server(httpServer, {
 });
 
 // Middleware
-app.use(cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials: true
-}));
+
 app.use(express.json());
 app.use(cookieParser());
 
